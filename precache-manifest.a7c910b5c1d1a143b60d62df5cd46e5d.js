7a5b874a20f29eab5895b7534a9d3f735d850865
adishthapa <adishthapa@outlook.com>
2019-10-18 18:41:24 -0400

    Testing Github build

---
+++ precache-manifest.a7c910b5c1d1a143b60d62df5cd46e5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcd1efc0782ac49e945df2d1a0b9eb7",
+    "url": "/UNCBootcamp-HW19-Clicky-Game/index.html"
+  },
+  {
+    "revision": "69b183bb94fd2c3dcbdb",
+    "url": "/UNCBootcamp-HW19-Clicky-Game/static/css/main.ecdc8087.chunk.css"
+  },
+  {
+    "revision": "49f7d682a7d9dfacf199",
+    "url": "/UNCBootcamp-HW19-Clicky-Game/static/js/2.4fe49865.chunk.js"
+  },
+  {
+    "revision": "69b183bb94fd2c3dcbdb",
+    "url": "/UNCBootcamp-HW19-Clicky-Game/static/js/main.5c4adc8d.chunk.js"
+  },
+  {
+    "revision": "7f489007adc781abb351",
+    "url": "/UNCBootcamp-HW19-Clicky-Game/static/js/runtime-main.d39ab6a3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/UNCBootcamp-HW19-Clicky-Game/precache-manifest.a7c910b5c1d1a143b60d62df5cd46e5d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/UNCBootcamp-HW19-Clicky-Game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
